52a93e1efeae2f0604ef6fd333cb25358fd0528e
lorijseo <lorijseo@gmail.com>
2022-02-17 21:09:55 -0800

    initialized repository

---
+++ HappyPikachu.py
@@ -0,0 +1,161 @@
+print('''
+                                        ,'\\
+          _.----.        ____         ,'  _\   ___    ___     ____
+      _,-'       `.     |    |  /`.   \,-'    |   \  /   |   |    \  |`.
+      \      __    \    '-.  | /   `.  ___    |    \/    |   '-.   \ |  |
+      \.    \ \   |  __  |  |/    ,','_  `.  |          | __  |    \|  |
+        \    \/   /,' _`.|      ,' / / / /   |          ,' _`.|     |  |
+          \     ,-'/  /   \    ,'   | \/ / ,`.|         /  /   \  |     |
+          \    \ |   \_/  |   `-.  \    `'  /|  |    ||   \_/  | |\    |
+            \    \ \      /       `-.`.___,-' |  |\  /| \      /  | |   |
+            \    \ `.__,'|  |`-._    `|      |__| \/ |  `.__,'|  | |   |
+              \_.-'       |__|    `-._ |              '-.|     '-.| |   |
+                                      `'                            '-._|
+    ''')
+print("Hello there trainer!Welcome to Viridian Forest!From here on out, you will be encountering some wild Pokemon.\n")
+print("Your goal is to have a positive training experience with Pikachu!\n ")
+pikachuHere = input("Is Pikachu here with you today? Yes or No? \n")
+pikachu = pikachuHere.lower()
+if pikachu == "yes":
+    print(''' 
+       \:.             .:/
+        \``._________.''/ 
+         \             / 
+ .--.--, / .':.   .':. \\
+/__:  /  | '::' . '::' |
+   / /   |`.   ._.   .'|
+  / /    |.'         '.|
+ /___-_-,|.\  \   /  /.|
+      // |''\.;   ;,/ '|
+      `==|:=         =:|
+         `.          .'
+           :-._____.-:
+          `''       `''')
+    print("Oh! I didn't notice you there, Pikachu! I hope you both are ready to encounter some wild Pokemon!\n ")
+    enterNow = input("Are you ready to enter the Viridian Forest? Yes or No? \n")
+    enter = enterNow.lower()
+    if enter == "yes":
+        print('''   ,,,                      ,,,
+       {{{}}    ,,,             {{{}}    ,,,
+    ,,, ~Y~    {{{}},,,      ,,, ~Y~    {{{}},,, 
+   {{}}} |/,,,  ~Y~{{}}}    {{}}} |/,,,  ~Y~{{}}}
+    ~Y~ \|{{}}}/\|/ ~Y~  ,,, ~Y~ \|{{}}}/\|/ ~Y~  ,,,
+    \|/ \|/~Y~  \|,,,|/ {{}}}\|/ \|/~Y~  \|,,,|/ {{}}}
+    \|/ \|/\|/  \{{{}}/  ~Y~ \|/ \|/\|/  \{{{}}/  ~Y~
+    \|/\\|/\|/ \\|~Y~//  \|/ \|/\\|/\|/ \\|~Y~//  \|/
+    \|//\|/\|/,\\|/|/|// \|/ \|//\|/\|/,\\|/|/|// \|/
+    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^''')
+        print("\nYou and Pikachu entered the Viridian Forest.\n"
+              "BUT WHAT'S THIS?\nA Wild Charzard is blocking your way!\n")
+        print('''                
+              ."-,.__
+                        `.     `.  ,
+                      .--'  .._,'"-' `.
+                    .    .'         `'
+                    `.   /          ,'
+                      `  '--.   ,-"'
+                        `"`   |  \\
+                          -. \, |
+                            `--Y.'      ___.
+                                \     L._, \\
+                      _.,        `.   <  <\                _
+                    ,' '           `, `.   | \            ( `
+                  ../, `.            `  |    .\`.           \ \_
+                ,' ,..  .           _.,'    ||\l            )  '".
+                , ,'   \           ,'.-.`-._,'  |           .  _._`.
+              ,' /      \ \        `' ' `--/   | \          / /   ..\\
+            .'  /        \ .         |\__ - _ ,'` `        / /     `.`.
+            |  '          ..         `-...-"  |  `-'      / /        . `.
+            | /           |L__           |    |          / /          `. `.
+          , /            .   .          |    |         / /             ` `
+          / /          ,. ,`._ `-_       |    |  _   ,-' /               ` \\
+        / .           \ "`_/. `-_ \_,.  ,'    +-' `-'  _,        ..,-.    \`.
+        |'      _.-""` `.    \ _,'  `            \ `.___`.'"`-.  , |   |    | \\
+        ||    ,'      `. `.   '       _,...._        `  |    `/ '  |   '     .|
+        ||  ,'          `. ;.,.---' ,'       `.   `.. `-'  .-' /_ .'    ;_   ||
+        || '              V      / /           `   | `   ,'   ,' '.    !  `. ||
+        ||/            _,-------7 '              . |  `-'    l         /    `||
+        . |          ,' .-   ,' ||               | .-.        `.      .'     ||
+        `'        ,'    `".'    |               |    `.        '. -.'       `'
+                  /      ,'      |               |,'    \-.._,.'/'
+                  .     /        .               .       \    .''
+                .`.    |         `.             /         :_,'.'
+                  \ `...\   _     ,'-.        .'         /_.-'
+                  `-.__ `,  `'   .  _.>----''.  _  __  /
+                        .'        /"'          |  "'   '_
+                      /_|.-'\ ,".             '.'`__'-( \\
+                        / ,"'"\,'               `/  `-.|"''')
+        do = input("Pikachu is ready to attack.\nTrainer, what do you say? QUICK ATTACK or THUNDER?\n ")
+        doNow = do.upper()
+
+        if doNow == "QUICK ATTACK":
+            print(" \nA critical hit!")
+            do2 = input("Pikachu is ready to attack again.\nTrainer, what do you say? IRON TAIL or THUNDER BOLT?\n ")
+            do2Now = do2.upper()
+
+            if do2Now == "THUNDER BOLT":
+                print(" \nIt's super effective! Wild Charzard is paralyzed! It can't move!\n ")
+                do3 = input(
+                    "What will you do, Trainer?\n"
+                    "say 'BAG' to throw a Pokeball\n"
+                    "say 'ATTACK' to continue battling\n"
+                    "say 'RUN' to leave this battle\n ")
+                do3Now = do3.upper()
+
+                if do3Now == "BAG":
+                    print('''      
+            |@@@@|     |####|
+            |@@@@|     |####|
+            |@@@@|     |####|
+            \@@@@|     |####/
+            \@@@|      |###/
+              `@@|_____|##'
+                   (O)
+                .-------.
+              .'  * * *  `.
+             :  *       *  :
+             : ~  HAPPY  ~ :
+             : ~ PIKACHU ~ :
+             :  *       *  :
+              `.  * * *  .'
+                `-.....-''')
+                    print("Gotcha! Charzard was caught!\nPikachu is happy and proud of your achievement!\nYou win!")
+                elif do3Now == "ATTACK":
+                    print(
+                        "Pikachu is exhausted. "
+                        "Embarassed by its lack of stamina, Pikachu's ego is hurt and doesn't want to continue further."
+                        "\nGame over.")
+                elif do3Now == "RUN":
+                    print(
+                        "Pikachu looks at you with disappointment. "
+                        "Pikachu's ego is hurt and doesn't want to continue further."
+                        "\nGame over.")
+                else:
+                    print(
+                        "Hm? What was that? While you were fumbling over your decision, Charzard flew away. "
+                        "Pikachu is not happy.\nGame over.")
+
+            elif do2Now == "IRON TAIL":
+                print(
+                    "It's not very effective. Wild Charzard scoffs and flies away.\n"
+                    "Pikachu's ego is hurt and doesn't want to continue further.\n"
+                    "Game over.")
+            else:
+                print(
+                    "Pikachu is confused and didn't understand your command.\n"
+                    "Embarassed at your lack of battle experience, Pikachu walks back home alone.\nGame over.")
+
+        elif doNow == "THUNDER":
+            print(
+                " \nTHUNDER MISSES. Wild Charzard lazily looks at you with disappointment and flies away.\n"
+                "Pikachu's ego is hurt and doesn't want to continue further.\nGame over.")
+        else:
+            print(
+                "Pikachu is confused and didn't understand your command.\n"
+                "mbarassed at your lack of battle experience, Pikachu walks back home alone.\nGame over.")
+    else:
+        print("Come again next time!")
+else:
+    print("Oh. You cannot enter this area without your Pikachu. Come again next time!")
+
+
